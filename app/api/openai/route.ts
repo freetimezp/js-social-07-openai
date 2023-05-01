211bbf871812ab54b656e6b55f9d3013cd304f10
freetimezp <kraftukrnet@ukr.net>
2023-05-01 18:58:54 +0300

    05. js-social-07-openai. Config and connect to OpenAI.

--- app/api/openai/route.ts
+++ app/api/openai/route.ts
@@ -1,4 +1,11 @@
 import { NextResponse } from "next/server";
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY
+});
+
+const openai = new OpenAIApi(configuration);
 
 export async function GET(request: Request) {
     return new Response("Hello, Form OpenAI");
@@ -6,8 +13,15 @@ export async function GET(request: Request) {
 
 export async function POST(request: Request) {
     const { userText } = await request.json();
-
     //console.log(userText);
 
-    return NextResponse.json({ message: userText }, { status: 200 });
+    const completion = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: [{ role: "user", content: userText }]
+    });
+
+    const aiMessage = completion.data.choices[0].message;
+    console.log(aiMessage?.content);
+
+    return NextResponse.json({ message: aiMessage?.content }, { status: 200 });
 }
