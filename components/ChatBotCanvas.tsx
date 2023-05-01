07d93fa62206894306a7c9ec7339bb3a0d68d6ad
freetimezp <kraftukrnet@ukr.net>
2023-05-01 21:43:43 +0300

    07. js-social-07-openai. Torch component.

--- components/ChatBotCanvas.tsx
+++ components/ChatBotCanvas.tsx
@@ -1,7 +1,36 @@
 "use client";
-import { OrbitControls, useGLTF, useAnimations } from '@react-three/drei';
+import { OrbitControls, SpotLight, useGLTF, useAnimations } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
+import { Vector3 } from 'three';
+
+const Torch = ({ vec = new Vector3(), ...props }) => {
+    const light = useRef<THREE.SpotLight>(null);
+
+    return (
+        <SpotLight
+            shadowCameraFov={undefined}
+            shadowCameraLeft={undefined}
+            shadowCameraRight={undefined}
+            shadowCameraTop={undefined}
+            shadowCameraBottom={undefined}
+            shadowCameraNear={undefined}
+            shadowCameraFar={undefined}
+            shadowBias={undefined}
+            shadowMapWidth={undefined}
+            shadowMapHeight={undefined}
+
+            ref={light}
+            castShadow
+            penumbra={1}
+            distance={10}
+            angle={0.35}
+            attenuation={5}
+            anglePower={4}
+            intensity={3}
+            {...props} />
+    );
+};
 
 const Head = () => {
     const model = useGLTF("/head.glb");
@@ -14,7 +43,13 @@ const Head = () => {
         //action?.stop();
     }, [action]);
 
-    return <primitive object={model.scene} scale={3} rotation-z={0.2} />;
+    return (
+        <>
+            <primitive object={model.scene} scale={3} rotation-z={0.2} />
+            <Torch color="blue" position={[3, 2, 2]} />
+            <Torch color="yellow" position={[-3, 2, 2]} />
+        </>
+    );
 };
 
 const ChatBotCanvas = () => {
@@ -27,7 +62,7 @@ const ChatBotCanvas = () => {
                 minAzimuthAngle={-Math.PI * 0.5}
                 maxAzimuthAngle={Math.PI * 0.5}
             />
-            <ambientLight />
+            <ambientLight intensity={0.015} />
             <Head />
         </Canvas>
     );
