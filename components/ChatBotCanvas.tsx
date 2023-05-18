c888f2b44939c7142f28820ee33410a4f15612fe
freetimezp <kraftukrnet@ukr.net>
2023-05-18 14:29:28 +0300

    11. js-social-07-openai. fix.

--- components/ChatBotCanvas.tsx
+++ components/ChatBotCanvas.tsx
@@ -1,86 +1,104 @@
 "use client";
-import { AppContext } from '@/app/context/IsPlayingContext';
-import { OrbitControls, SpotLight, useGLTF, useAnimations } from '@react-three/drei';
-import { Canvas, useFrame, useThree } from '@react-three/fiber';
-import React, { useEffect, useRef, useContext } from 'react';
-import { Vector3 } from 'three';
+import { AppContext } from "../app/context/IsPlayingContext";
+import {
+	Environment,
+	Html,
+	Loader,
+	OrbitControls,
+	SpotLight,
+	useAnimations,
+	useDepthBuffer,
+	useGLTF,
+} from "@react-three/drei";
+import { Canvas, useFrame, useThree } from "@react-three/fiber";
+import { Suspense, useContext, useEffect, useRef } from "react";
+import { Vector3 } from "three";
 
 const Torch = ({ vec = new Vector3(), ...props }) => {
-    const light = useRef<THREE.SpotLight>(null);
-    const viewport = useThree(state => state.viewport);
-
-    useFrame((state) => {
-        light.current?.target.position.lerp(
-            vec.set((state.mouse.x * viewport.width) / 2, (state.mouse.y * viewport.width) / 2, 0), 0.1
-        );
-        light.current?.target.updateMatrixWorld();
-    });
-
-    return (
-        <SpotLight
-            shadowCameraFov={undefined}
-            shadowCameraLeft={undefined}
-            shadowCameraRight={undefined}
-            shadowCameraTop={undefined}
-            shadowCameraBottom={undefined}
-            shadowCameraNear={undefined}
-            shadowCameraFar={undefined}
-            shadowBias={undefined}
-            shadowMapWidth={undefined}
-            shadowMapHeight={undefined}
-
-            ref={light}
-            castShadow
-            penumbra={1}
-            distance={10}
-            angle={0.35}
-            attenuation={5}
-            anglePower={4}
-            intensity={3}
-            {...props} />
-    );
+	const light = useRef<THREE.SpotLight>(null);
+	const viewport = useThree((state) => state.viewport);
+	useFrame((state) => {
+		light.current?.target.position.lerp(
+			vec.set(
+				(state.mouse.x * viewport.width) / 2,
+				(state.mouse.y * viewport.height) / 2,
+				0
+			),
+			0.1
+		);
+		light.current?.target.updateMatrixWorld();
+	});
+	return (
+		<SpotLight
+			castShadow
+			ref={light}
+			penumbra={1}
+			distance={10}
+			angle={0.35}
+			attenuation={5}
+			anglePower={4}
+			intensity={3}
+			{...props}
+		/>
+	);
 };
 
+//Resource to Head : https://sketchfab.com/3d-models/blender-sushi-virtual-journal-16th-april-2020-634af2ae983f4fb8a9295e6b1b3d5c74
 const Head = () => {
-    const model = useGLTF("/head.glb");
-    const animation = useAnimations(model.animations, model.scene);
-    const action = animation.actions.Animation;
-    const { isPlaying, setIsPlaying } = useContext(AppContext);
+	const { isPlaying, setIsPlaying } = useContext(AppContext);
+	const model = useGLTF("/head.glb");
+	const animations = useAnimations(model.animations, model.scene);
+	const action = animations.actions.Animation;
+	const depthBuffer = useDepthBuffer({ frames: 1 });
+	useEffect(() => {
+		if (isPlaying) {
+			action?.play();
+		} else {
+			action?.fadeOut(0.5);
+			setTimeout(() => {
+				action?.stop();
+			}, 500);
+		}
+	}, [isPlaying, action]);
 
-    //console.log(action);
-
-    useEffect(() => {
-        if (isPlaying) {
-            action?.play();
-        } else {
-            action?.fadeOut(0.5);
-            setTimeout(() => {
-                action?.stop();
-            }, 500);
-        }
-    }, [isPlaying, action]);
-
-    return (
-        <>
-            <primitive object={model.scene} scale={3} rotation-z={0.2} />
-            <Torch color="blue" position={[3, 2, 2]} />
-            <Torch color="yellow" position={[-3, 2, 2]} />
-        </>
-    );
+	return (
+		<>
+			<primitive object={model.scene} scale={3} rotation-z={0.2} />
+			<Torch
+				depthBuffer={depthBuffer}
+				color="blue"
+				position={[3, 2, 2]}
+			/>
+			<Torch
+				depthBuffer={depthBuffer}
+				color="#b00c3f"
+				position={[-3, 2, 2]}
+			/>
+		</>
+	);
 };
 
 export const ChatBotCanvas = () => {
-    return (
-        <Canvas>
-            <OrbitControls
-                enableZoom={false}
-                enableDamping
-                maxPolarAngle={2}
-                minAzimuthAngle={-Math.PI * 0.5}
-                maxAzimuthAngle={Math.PI * 0.5}
-            />
-            <ambientLight intensity={0.015} />
-            <Head />
-        </Canvas>
-    );
-}
+	return (
+		<Canvas>
+			<OrbitControls
+				enableZoom={false}
+				enableDamping
+				maxPolarAngle={2}
+				minAzimuthAngle={-Math.PI * 0.5}
+				maxAzimuthAngle={Math.PI * 0.5}
+			/>
+			<color attach="background" args={["black"]} />
+			<ambientLight intensity={0.015} />
+			<Suspense
+				fallback={
+					<Html>
+						<Loader />
+					</Html>
+				}
+			>
+				<Head />
+			</Suspense>
+		</Canvas>
+	);
+};
--- components/TextToSpeech.tsx
+++ components/TextToSpeech.tsx
@@ -1,79 +1,78 @@
 "use client";
-import { AppContext } from '@/app/context/IsPlayingContext';
-import { sendTextToOpenAi } from '@/utils/sendTextToOpenAI';
-import React, { FormEvent, useState, useContext } from 'react';
-
+import { AppContext } from "../app/context/IsPlayingContext";
+import { sendTextToOpenAi } from "@/utils/sendTextToOpenai";
+import React, { FormEvent, useContext, useState } from "react";
+import yt from "../public/yt.webp";
+import Link from "next/link";
+import Image from "next/image";
 export const TextToSpeech = () => {
-    const [userText, setUserText] = useState("");
-    const [isLoading, setIsLoading] = useState(false);
-    const { isPlaying, setIsPlaying } = useContext(AppContext);
+	const [userText, setUserText] = useState("");
+	const [isLoading, setIsLoading] = useState(false);
+	const { isPlaying, setIsPlaying } = useContext(AppContext);
+	const synth = typeof window !== "undefined" ? window.speechSynthesis : null;
+	const voices = synth?.getVoices();
 
-    const synth = typeof window !== "undefined" ? window.speechSynthesis : null;
-    const voices = synth?.getVoices();
-    //console.log(voices);
+	const seletedVoice = voices?.find((voice) => voice.name === "Albert"); // Other voice that sounds good Karen, Tessa, Trinoids
 
-    const selectedVoices = voices?.find((voice) => voice.name === "Microsoft Zira - English (United States)");
-    //console.log(selectedVoices);
+	const speak = (textToSpeak: string) => {
+		const utterance = new SpeechSynthesisUtterance(textToSpeak);
+		utterance.rate = 0.5;
+		utterance.voice = seletedVoice!;
 
-    const speak = (textToSpeak: string) => {
-        const utterance = new SpeechSynthesisUtterance(textToSpeak);
-        utterance.voice = selectedVoices!;
-        utterance.rate = 0.5;
+		synth?.speak(utterance);
+		setIsPlaying(true);
+		utterance.onend = () => {
+			setIsPlaying(false);
+		};
+	};
 
-        synth?.speak(utterance);
-        setIsLoading(true);
+	async function handleUserText(event: FormEvent<HTMLFormElement>) {
+		event.preventDefault();
+		if (userText === "") return alert("Please enter text");
+		setIsLoading(true);
+		try {
+			const message = await sendTextToOpenAi(userText);
+			speak(message);
+		} catch (error) {
+			let message = "";
+			if (error instanceof Error) message = error.message;
+			console.log(message);
+		} finally {
+			setIsLoading(false);
+			setUserText("");
+		}
+	}
 
-        setIsPlaying(true);
-
-        utterance.onend = () => {
-            setIsLoading(false);
-            setIsPlaying(false);
-        }
-    };
-
-    const handleUserText = async (e: FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        setIsLoading(true);
-        //console.log(userText);
-
-        try {
-            const message = await sendTextToOpenAi(userText);
-            speak(message);
-        } catch (error) {
-            let message = "";
-            if (error instanceof Error) {
-                message = error.message;
-            }
-            //console.log(message);
-        } finally {
-            setIsLoading(false);
-            setUserText("");
-        };
-    };
-
-    return (
-        <div className='relative top-0 z-50'>
-            <form
-                className='absolute -top-[120px] left-[10px] space-x-2 pt-2'
-                onSubmit={handleUserText}
-            >
-                <input
-                    value={userText}
-                    onChange={(e) => setUserText(e.target.value)}
-                    className='bg-transparent sm:w-[550px] w-[290px] border border-[#b00c3f]/80 outline-none rounded-lg
-                        placeholder:text-[#b00c3f] p-2 text-[#b00c3f]'
-                    placeholder='What do you want to know human?...'
-                    type="text"
-                />
-                <button
-                    className='text-[#b00c3f] py-2 px-4 border border-[#b00c3f] rounded-lg disabled:text-blue-100
-                    disabled:cursor-not-allowed disabled:bg-gray hover:text-black hover:bg-[#b00c3f]
-                    duration-300 transition-all'
-                    disabled={isLoading}
-                >
-                    {isLoading ? "Thinking" : "Ask"}
-                </button>
-            </form>
-        </div>
-    );
-}
+	return (
+		<div className="relative top-0 z-50 ">
+			<form
+				onSubmit={handleUserText}
+				className="absolute top-[800px] left-[30%]  space-x-2 pt-2 "
+			>
+				<input
+					type="text"
+					value={userText}
+					className="bg-transparent w-[510px] border border-[#b00c3f]/80 outline-none  rounded-lg placeholder:text-[#b00c3f] p-2 text-[#b00c3f]"
+					onChange={(e) => setUserText(e.target.value)}
+					placeholder="What do you want to know human...."
+				/>
+				<button
+					disabled={isLoading}
+					className="text-[#b00c3f] p-2 border border-[#b00c3f] rounded-lg disabled:text-blue-100 
+					disabled:cursor-not-allowed disabled:bg-gray-500 hover:scale-110 hover:bg-[#b00c3f] hover:text-black duration-300 transition-all"
+				>
+					{isLoading ? "thinking..." : "Ask"}
+				</button>
+			</form>
+			<div className="absolute top-3 right-3 ">
+				<Link
+					target="_blank"
+					href={"https://www.youtube.com/@developertak2634"}
+				>
+					<Image src={yt} alt="yt" height={50} width={50} />
+				</Link>
+				<div className="absolute top-0 bg-black/60" />
+			</div>
+		</div>
+	);
+};
